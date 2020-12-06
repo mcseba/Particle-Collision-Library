672c55be87bba759e398eeca742139fc810d56ef
mcseba <48526736+mcseba@users.noreply.github.com>
2020-12-06 16:13:54 +0100

    properties to manage collision system

--- ParticleCollisionSystem/ParticleCollisionSystem/CollisionSystem.cs
+++ ParticleCollisionSystem/ParticleCollisionSystem/CollisionSystem.cs
@@ -6,5 +6,25 @@ namespace ParticleCollisionSystem
 {
     class CollisionSystem
     {
+        #region props
+        /// <summary>
+        /// Number of event redraws per clock tick
+        /// </summary>
+        private static readonly double HZ = 0.5;
+        /// <summary>
+        /// Priority Queue of events
+        /// </summary>
+        private PriorityQueue<Event> pq;
+        /// <summary>
+        /// simulation time
+        /// </summary>
+        private double t = 0.0;
+        /// <summary>
+        /// Array of particles
+        /// </summary>
+        private Particle[] particles;
+        #endregion
+
+
     }
 }
